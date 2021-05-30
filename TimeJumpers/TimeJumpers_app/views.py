0091e02701f850115bf3bf2c2ada1bd3c595d0e3
jjmcinto <jjmcinto@andrew.cmu.edu>
2021-05-30 14:27:46 -0700

    gave up on Google speech-to-text; now running AssemblyAI, which actually works

--- TimeJumpers/TimeJumpers_app/views.py
+++ TimeJumpers/TimeJumpers_app/views.py
@@ -1,61 +1,69 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import io, requests, json
+#dbWords = {};
 
-def transcribe_gcs_with_word_time_offsets(gcs_uri):
-    """Transcribe the given audio file asynchronously and output the word time
-    offsets."""
-    from google.cloud import speech
-    import os, datetime
-    
-    #print("pre-set 'client'!");
-    #export GOOGLE_APPLICATION_CREDENTIALS = "";
-    #export GOOGLE_APPLICATION_CREDENTIALS = "../../../arcane-geode-315302-b55530bd6b4f.json";
-    #call("export GOOGLE_APPLICATION_CREDENTIALS = ../../../arcane-geode-315302-b55530bd6b4f.json");
-    #call("export GOOGLE_APPLICATION_CREDENTIALS='../../../arcane-geode-315302-b55530bd6b4f.json'");
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS']= "/Users/vdo/Documents/Jeffrey/CMU/Courses/2021T2/49783/arcane-geode-315302-1851ff0299a3.json";
-    os.environ['MAX_CONTENT_LENGTH']="16000";
-    #putenv("GOOGLE_APPLICATION_CREDENTIALS", "../../../arcane-geode-315302-b55530bd6b4f.json");
-    #print("nonsense");
-    #print(os.environ);
-    client = speech.SpeechClient(); #internal server error!
-    
-    audio = speech.RecognitionAudio(uri=gcs_uri)
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
-        sample_rate_hertz=16000,
-        language_code="en-US",
-        enable_word_time_offsets=True,
-    )
-    
-    operation = client.long_running_recognize(config=config, audio=audio)
-    
-    print(str(datetime.datetime.now()));
-    print("Waiting for operation to complete...");
-    result = operation.result(timeout=900);
-    print("Result retrieved!");
-    print(str(datetime.datetime.now()));
-    print(result.results);
-    
-    for result in result.results:
-        alternative = result.alternatives[0]
-        print("Transcript: {}".format(alternative.transcript))
-        print("Confidence: {}".format(alternative.confidence))
+def transcribe_assemblyai(auth: str): #9 dollars to process 10 hours of input
+    endpoint = "https://api.assemblyai.com/v2/transcript"
+
+    json = {
+      "audio_url": "https://s3-us-west-2.amazonaws.com/blog.assemblyai.com/audio/8-7-2018-post/7510.mp3"
+    }
+
+    headers = {
+        "authorization": auth,
+        "content-type": "application/json"
+    }
+
+    response = requests.post(endpoint, json=json, headers=headers)
+
+    print("JSON response:", response.json());
+    return response.json();
+
+def query_transcript(transcriptID: str, auth: str):
+    #sometimes returns OSError: [Errno 41] Protocol wrong type for socket
+    #   in such cases, retry
+    dbWords = None;
+    endpoint = "https://api.assemblyai.com/v2/transcript/" + transcriptID;
+    headers = {
+        "authorization": auth,
+    };
+    while not dbWords:
+        response = requests.get(endpoint, headers=headers);
         
-        for word_info in alternative.words:
-            word = word_info.word
-            start_time = word_info.start_time
-            end_time = word_info.end_time
-            
-            print(
-                f"Word: {word}, start_time: {start_time.total_seconds()}, end_time: {end_time.total_seconds()}"
-            )
-
+        #return response.json();
+        dbWords = response.json()['words'];
+    print("query_transcript response:", response.json());
+    print("dbWords:", dbWords);
+    return dbWords;
+    
+def findAll(keyword: str, dbWords: list[dict]) -> list[int]:
+    r = [];
+    #print("dbWords in findAll:", dbWords);
+    print("finding '" + keyword + "'");
+    for d in dbWords:
+        print("d['text']:", d['text']);
+        if d['text'].find(keyword) >= 0:
+            r.append(d['start']);
+    return r;
 
 # Create your views here.
 def index(request):
+    transcriptID = "jixtkhcr6-0aba-4092-a235-db2744adfe04";
+    auth = "9ce7bcff260346dcb2810fa76023732b";
+    #transcriptID = transcribe_assemblyai(auth)['id'];
+    #query_transcript(transcriptID, auth);
+    dbWords = query_transcript(transcriptID, auth); #list of dictionaries
+    #print("id:", dScript["id"]);
+    
+    #return HttpResponse("Where can I upload, eh?<br>");
+    
+    return HttpResponse("Where can I upload, eh?<br>" + str(dbWords) + "<br>" + "know: " + str(findAll("know", dbWords)));
+    
     #transcribe_gcs_with_word_time_offsets("file:///Users/vdo/Desktop/lectureIntro.mov"); #"invalid GCS path"
     #transcribe_gcs_with_word_time_offsets("/Users/vdo/Desktop/lectureIntro.mov");
     #transcribe_gcs_with_word_time_offsets("gs://49783_input/LectureIntrd.mp4"); #fileNotFound error
-    #transcribe_gcs_with_word_time_offsets("gs://49783_input/LectureIntro.mp4");
-    transcribe_gcs_with_word_time_offsets("gs://49783_input/enunciate.mp4");
-    return HttpResponse('Where can I upload, eh?');
+    #transcribe_gcs_with_word_time_offsets("gs://49783_input/LectureIntro.mp4"); #empty output
+    #transcribe_gcs_with_word_time_offsets("gs://49783_input/enunciate.mp4"); #empty output
+    #transcribe_gcs_with_word_time_offsets("gs://49783_input/audiu.raw"); #404 No such object - so, at least the above lines are finding the input!
+    #transcribe_assemblyai();
