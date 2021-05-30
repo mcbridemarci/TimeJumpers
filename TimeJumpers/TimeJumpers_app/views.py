b9941db583947bc14c5dce97a680d536136f95fa
jjmcinto <jjmcinto@andrew.cmu.edu>
2021-05-30 15:06:42 -0700

    figured out how to scan output for given keyword; output time of each occurrence

--- TimeJumpers/TimeJumpers_app/views.py
+++ TimeJumpers/TimeJumpers_app/views.py
@@ -3,12 +3,16 @@ from django.http import HttpResponse
 import io, requests, json
 #dbWords = {};
 
-def transcribe_assemblyai(auth: str): #9 dollars to process 10 hours of input
+def transcribe_assemblyai(auth: str, audio_url: str): #9 dollars to process 10 hours of input
     endpoint = "https://api.assemblyai.com/v2/transcript"
 
+    #json = {
+    #  "audio_url": "https://s3-us-west-2.amazonaws.com/blog.assemblyai.com/audio/8-7-2018-post/7510.mp3"
+    #}
+    
     json = {
-      "audio_url": "https://s3-us-west-2.amazonaws.com/blog.assemblyai.com/audio/8-7-2018-post/7510.mp3"
-    }
+      "audio_url": audio_url
+      }
 
     headers = {
         "authorization": auth,
@@ -49,16 +53,19 @@ def findAll(keyword: str, dbWords: list[dict]) -> list[int]:
 
 # Create your views here.
 def index(request):
-    transcriptID = "jixtkhcr6-0aba-4092-a235-db2744adfe04";
     auth = "9ce7bcff260346dcb2810fa76023732b";
-    #transcriptID = transcribe_assemblyai(auth)['id'];
-    #query_transcript(transcriptID, auth);
+    audio_url = "https://storage.googleapis.com/49783_input/LectureIntro.mp4";
+    transcriptID = "jixtkhcr6-0aba-4092-a235-db2744adfe04"; #default AssemblyAI input
+    #transcriptID = "jilog6fau-2d87-4ad4-a8d3-fd795ee4d06f"; #LectureIntro.mp4
+    
+    #transcriptID = transcribe_assemblyai(auth, audio_url)['id'];
+    print("transcriptID:", transcriptID);
     dbWords = query_transcript(transcriptID, auth); #list of dictionaries
-    #print("id:", dScript["id"]);
     
     #return HttpResponse("Where can I upload, eh?<br>");
-    
-    return HttpResponse("Where can I upload, eh?<br>" + str(dbWords) + "<br>" + "know: " + str(findAll("know", dbWords)));
+    searchWord = "know";
+    pos = findAll(searchWord, dbWords);
+    return HttpResponse("Where can I upload, eh?<br>" + str(dbWords) + "<br>" + searchWord + ": " + (str(pos) if pos else "(none)"));
     
     #transcribe_gcs_with_word_time_offsets("file:///Users/vdo/Desktop/lectureIntro.mov"); #"invalid GCS path"
     #transcribe_gcs_with_word_time_offsets("/Users/vdo/Desktop/lectureIntro.mov");
