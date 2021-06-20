54c8871d4ebd86afcf446c5e5ee32de1484b0e66
jjmcinto <jjmcinto@andrew.cmu.edu>
2021-06-20 10:06:50 -0700

    add password in settings.py to connect to database hosted on AWS

--- TimeJumpers/TimeJumpers/settings.py
+++ TimeJumpers/TimeJumpers/settings.py
@@ -75,13 +75,24 @@ WSGI_APPLICATION = 'TimeJumpers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'timejumper',
+#        'USER': 'postgres',
+#        'PASSWORD': '1234',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'timejumper',
         'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
+        'PASSWORD': '***',
+        'HOST': 'timejumpers.cdfbjmjoumet.us-east-1.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
